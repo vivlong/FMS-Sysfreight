1f45f0c964c7ee91da8369fb98ed0bb05be7313d
vivlong <vivlonglz@gmail.com>
2015-12-08 10:29:18 +0800

    Change VesselSchedule-detail.html AS Dorcas required.

--- FreightMob/www/js/webconfig.js
+++ FreightMob/www/js/webconfig.js
@@ -5,9 +5,12 @@ var strWebSiteURL = "www.sysfreight.net:8081/mobileapp";
 var strSecretKey = "9CBA0A78-7D1D-49D3-BA71-C72E93F9E48F";
 
 Date.prototype.Format = function (fmt) {
+	var Month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     var o = {
-        "M+": this.getMonth() + 1,
+        "M+": this.getMonth() + 1,		
+        "NNN": Month[this.getMonth()],
         "d+": this.getDate(),
+        "H+": this.getHours(),
         "h+": this.getHours(),
         "m+": this.getMinutes(),
         "s+": this.getSeconds(),
@@ -16,7 +19,14 @@ Date.prototype.Format = function (fmt) {
     };
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
     for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	{
+		if(k==='NNN'){
+			if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length-1)));
+		}
+		else{
+			if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+		}
+	}
     return fmt;
 }
 
