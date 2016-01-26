d37b38b7c3cb19fac64a879cf9c4e2031a50fb43
vivlong <vivlonglz@gmail.com>
2016-01-26 15:30:09 +0800

    Update Contacts-info.html Contacts-info-Edit.html Contacts-info-Add.html layout

--- FreightMob/www/js/app.js
+++ FreightMob/www/js/app.js
@@ -224,25 +224,24 @@ app.config(['$stateProvider', '$urlRouterProvider', '$ionicConfigProvider',
                 controller: 'ContactsListCtrl'
             })
             .state('contactsDetail', {
-                url: '/contacts/detail/:TrxNo/:BusinessPartyNameLike',
-                cache: 'false',
+                url: '/contacts/detail/:TrxNo',
                 templateUrl: 'view/crm/Contacts-detail.html',
                 controller: 'ContactsDetailCtrl'
             })
             .state('contactsDetailEdit', {
-                url: '/contacts/detail/Edit/:TrxNo/:BusinessPartyName',
+                url: '/contacts/detail/Edit',
                 cache: 'false',
                 templateUrl: 'view/crm/Contacts-detail-Edit.html',
                 controller: 'ContactsDetailEditCtrl'
             })
             .state('contactsInfo', {
-                url: '/contacts/info/:BusinessPartyCode/:LineItemNo',
+                url: '/contacts/info',
                 cache: 'false',
                 templateUrl: 'view/crm/Contacts-info.html',
                 controller: 'ContactsInfoCtrl'
             })
             .state('contactsInfoEdit', {
-                url: '/contacts/info/Edit/:BusinessPartyCode/:LineItemNo',
+                url: '/contacts/info/Edit',
                 cache: 'false',
                 templateUrl: 'view/crm/Contacts-info-Edit.html',
                 controller: 'ContactsInfoEditCtrl'
